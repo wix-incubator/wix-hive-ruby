3300f9880aae2bc5df1d7fe659d6d44bf4176072
Alex Tomovski <alext@wix.com>
2014-08-05 11:46:23 +0300

    more tests

--- lib/wix/hive/connect/response/parse_json.rb
+++ lib/wix/hive/connect/response/parse_json.rb
@@ -7,17 +7,17 @@ module Wix
       class ParseJson < Faraday::Response::Middleware
         WHITESPACE_REGEX = /\A^\s*$\z/
 
-        def parse(body)
-          case body
-          when WHITESPACE_REGEX, nil
-            nil
-          else
-            JSON.parse(body, :symbolize_names => true)
-          end
-        end
-
         def on_complete(response)
           response.body = parse(response.body) if respond_to?(:parse) && !unparsable_status_codes.include?(response.status)
+        end
+
+        def parse(body)
+          case body
+            when WHITESPACE_REGEX, nil
+              nil
+            else
+              JSON.parse(body, :symbolize_names => true)
+          end
         end
 
         def unparsable_status_codes
