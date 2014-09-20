d363fb5c6f10e5a5ae89b309abfa53efbc62b099
Alex Tomovski <alext@wix.com>
2014-09-20 12:49:01 +0300

    ruby 1.9.3 rubocop fix

--- lib/hive/extensions/hashie_validate_enum.rb
+++ lib/hive/extensions/hashie_validate_enum.rb
@@ -1,6 +1,8 @@
 module Hashie
   module Validate
+
     module_function
+
     def enum(enum)
       lambda do |v|
         fail ArgumentError, "Invalid value #{v} ! Valid ones are: #{enum}" unless enum.include?(v)
