b856388e06ad851dc3c2fd7df6da0cbab086f00e
Karen Cohen <karenc@wix.com>
2015-02-19 19:34:40 +0200

    Increased coverage, now at 100%
    FYI The status-change event has been renamed to status-changed. Server guys still have to fix this.

--- spec/hive/activities/shipping/shipping_shipped_acitivty_spec.rb
+++ spec/hive/activities/shipping/shipping_shipped_acitivty_spec.rb
@@ -7,4 +7,15 @@ describe 'SHIPPING_SHIPPED' do
   it '.add_item' do
     create_activity.add_item( id: 1, title: 'title', quantity: 1, currency: 'EUR' )
   end
+
+  context 'SHIPPING_SHIPPED::Item' do
+    it '.add_variant' do
+      create_activity.add_item( id: 1, title: 'title', quantity: 1, currency: 'EUR' )
+      create_activity.items.first.add_variant( title: 'title' )
+    end
+    it '.add_metadata' do
+      create_activity.add_item( id: 1, title: 'title', quantity: 1, currency: 'EUR' )
+      create_activity.items.first.add_metadata( name: 'custom', value: 'value' )
+    end
+  end
 end
